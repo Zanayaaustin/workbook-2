f329798d53a6a3e1ddb4020fc2416a82d4ff1f09
Zanaya Austin <zanayaaustin427@gmail.com>
2023-04-24 16:42:23 -0400

    js 1st

---
+++ program.js
@@ -0,0 +1,11 @@
+var firstName;
+firstName = "Zanaya";
+lastName = "Austin";
+console.log(" hello world " + firstName + " " + lastName);
+
+var customerId - 555;
+var customerfirstName ="Tokyo"
+var customerlastName ="Hornandez"
+console.log(customerId);
+console.log(customerFirstName);
+console.log(customerlastName);
