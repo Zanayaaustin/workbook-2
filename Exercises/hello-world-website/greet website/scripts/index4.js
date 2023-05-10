3f203d6e942df94043d6a7c54465bd6b25281aad
Zanaya Austin <zanayaaustin427@gmail.com>
2023-05-10 10:21:38 -0400

    goin thru chnges

--- Exercises/hello-world-website/greet website/scripts/index4.js
+++ Exercises/hello-world-website/greet website/scripts/index4.js
@@ -2,8 +2,8 @@
 
 function greet() {
   const firstNameElement = document.getElementById("nameField");
-  const messageParagrapghElement = document.getElementById("messageParagraph");
+  const messageParagraphElement = document.getElementById("messageParagraph");
   const firstName = firstNameElement.value;
   let greeting = `Hello Beautiful ${firstName}`;
-  messageParagrapghElement.innerText = greeting;
+  messageParagraphElement.innerText = greeting;
 }
